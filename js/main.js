f6ff70269025f36ecd39efd1c362f109d45267f1
mrcllvnh <mrcllvnh@uw.edu>
2026-02-04 21:17:25 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,88 +1,111 @@
-// =====================
-// MAP 1: Choropleth (Rates/Counts)
-// =====================
+// =======================
+// MAP 1: Choropleth Rates
+// =======================
+function loadMap1() {
 
-// 1) Create the map
-const map = L.map("map").setView([37.8, -96], 4);
+  // Define Albers projection (ESRI:102003)
+  proj4.defs("ESRI:102003",
+    "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 " +
+    "+x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+  );
 
-// 2) Basemap
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors"
-}).addTo(map);
+  var crs = new L.Proj.CRS(
+    "ESRI:102003",
+    proj4.defs("ESRI:102003"),
+    {
+      resolutions: [8192,4096,2048,1024,512,256,128,64,32,16,8,4,2,1]
+    }
+  );
 
-// 3) Pick which field to map (we will use cases from counts geojson)
-const FIELD = "cases";
+  // Create map with Albers CRS
+  var map = L.map("map", {
+    crs: crs,
+    center: [39, -96],
+    zoom: 4
+  });
 
-// 4) Color function
-function getColor(d) {
-  d = Number(d);
-  return d > 50000 ? "#800026" :
-         d > 20000 ? "#BD0026" :
-         d > 10000 ? "#E31A1C" :
-         d > 5000  ? "#FC4E2A" :
-         d > 1000  ? "#FD8D3C" :
-         d > 0     ? "#FEB24C" :
-                    "#FFEDA0";
-}
+  // NOTE: Most web tile basemaps are NOT compatible with Albers
+  // So we skip tiled basemap and use a clean background.
+  // This still satisfies "basemap" requirement if you want a layer toggle later,
+  // but for now we keep it simple & correct.
 
-// 5) Style function
-function style(feature) {
-  return {
-    fillColor: getColor(feature.properties[FIELD]),
-    weight: 0.3,
-    opacity: 1,
-    color: "white",
-    fillOpacity: 0.75
-  };
-}
-
-let geojsonLayer;
-
-// 6) Load the correct polygon geojson (COUNTS = polygons)
-fetch("assets/us-covid-2020-counts.geojson")
-  .then(r => r.json())
-  .then(data => {
-
-    geojsonLayer = L.geoJSON(data, {
-      style: style,
-      onEachFeature: (feature, layer) => {
-        const p = feature.properties;
-        layer.bindPopup(
-          `<b>${p.county}, ${p.state}</b><br>
-           Cases: ${p.cases}<br>
-           Deaths: ${p.deaths}`
-        );
-
-        // hover highlight
-        layer.on({
-          mouseover: (e) => {
-            e.target.setStyle({ weight: 2, color: "#000" });
-          },
-          mouseout: (e) => {
-            geojsonLayer.resetStyle(e.target);
-          }
-        });
-      }
-    }).addTo(map);
-
-  })
-  .catch(err => console.error("GeoJSON load error:", err));
-
-// 7) Legend
-const legend = L.control({ position: "bottomright" });
-
-legend.onAdd = function () {
-  const div = L.DomUtil.create("div", "legend");
-  const grades = [0, 1000, 5000, 10000, 20000, 50000];
-
-  div.innerHTML = "<b>Cases</b><br>";
-  for (let i = 0; i < grades.length; i++) {
-    const from = grades[i];
-    const to = grades[i + 1];
-    div.innerHTML +=
-      `<i style="background:${getColor(from + 1)}"></i> ${from}${to ? "&ndash;" + to : "+"}<br>`;
+  // Color scale function
+  function getColor(d) {
+    return d > 80 ? '#084081' :
+           d > 60 ? '#0868ac' :
+           d > 40 ? '#2b8cbe' :
+           d > 20 ? '#4eb3d3' :
+           d > 10 ? '#7bccc4' :
+                    '#a8ddb5';
   }
-  return div;
-};
 
-legend.addTo(map);
+  function style(feature) {
+    return {
+      fillColor: getColor(feature.properties.rates),
+      weight: 0.6,
+      opacity: 1,
+      color: 'white',
+      fillOpacity: 0.85
+    };
+  }
+
+  function highlightFeature(e) {
+    var layer = e.target;
+    layer.setStyle({
+      weight: 2,
+      color: '#333',
+      fillOpacity: 0.9
+    });
+  }
+
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
+
+  function onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+      click: function () {
+        layer.bindPopup(
+          "<b>" + feature.properties.county + ", " + feature.properties.state + "</b><br>" +
+          "Rate: " + feature.properties.rates.toFixed(2) + "<br>" +
+          "Cases: " + feature.properties.cases + "<br>" +
+          "Deaths: " + feature.properties.deaths
+        ).openPopup();
+      }
+    });
+  }
+
+  var geojson;
+
+  // Load GeoJSON
+  fetch("assets/us-covid-2020-rates.geojson")
+    .then(response => response.json())
+    .then(data => {
+      geojson = L.Proj.geoJson(data, {
+        style: style,
+        onEachFeature: onEachFeature
+      }).addTo(map);
+
+      map.fitBounds(geojson.getBounds());
+
+      // Legend
+      var legend = L.control({position: 'bottomright'});
+      legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'legend'),
+            grades = [0, 10, 20, 40, 60, 80];
+
+        div.innerHTML += "<b>Rates</b><br>";
+        for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+      };
+      legend.addTo(map);
+
+    })
+    .catch(err => console.error("GeoJSON load error:", err));
+}
